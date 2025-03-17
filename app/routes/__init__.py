9fe84b4be9e28f8a8a334a474ce0e363636dae08
Kaung SiThu <kaungsithu.kaungsithu@gmail.com>
2025-03-17 23:45:48 +0630

    remove restructure files

--- app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-# Import all route modules to make them available when importing the routes package
-from app.routes import home
-from app.routes import users
-from app.routes import user_groups
-from app.routes import user_roles
-from app.routes import roles
-from app.routes import role_nested_roles
-from app.routes import role_privileges
-from app.routes import role_schema
--- main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from app import serve_app
-
-if __name__ == '__main__':
-    serve_app()
